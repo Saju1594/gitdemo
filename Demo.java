9bf6919112c47f7ac2425f37f476e1e23e8ce474
Edureka <edureka@example.com>
2021-11-10 04:51:21 +0000

    my 3rd commit

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
 	public static void main(Strings arg[]){
 		System.out.println("Hello World");
-		System.out.println("Learning world");
+		System.out.println("Learning world and DeveOPs");
 	}
 }
